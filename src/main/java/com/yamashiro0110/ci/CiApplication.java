94a260d4e81fe52443a1982953fd1155f5a8efc6
yamashiro.ryota <yamashiro0110@gmail.com>
2017-08-11 13:01:27 +0900

    add spring boot project

---
+++ src/main/java/com/yamashiro0110/ci/CiApplication.java
@@ -0,0 +1,12 @@
+package com.yamashiro0110.ci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/yamashiro0110/ci/CiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yamashiro0110.ci;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
